478ee4993adbcfdc5aa3ece869777bdc8c59f7bf
Marek <killermm@gmail.com>
2018-08-12 12:04:53 +0200

    initial Spring Boot 2 Web project

---
+++ src/test/java/pl/entito/spring/boot2/demo/web/SpringBootWebDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.entito.spring.boot2.demo.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
