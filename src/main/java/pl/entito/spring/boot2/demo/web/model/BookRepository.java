33670171f61b6152dd45886cebcde54becfa6a42
Marek <killermm@gmail.com>
2018-08-13 16:35:44 +0200

    JPA findBy two fields

--- src/main/java/pl/entito/spring/boot2/demo/web/model/BookRepository.java
+++ src/main/java/pl/entito/spring/boot2/demo/web/model/BookRepository.java
@@ -1,8 +1,12 @@
 package pl.entito.spring.boot2.demo.web.model;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
-	Book findByTitle(String title);
+	public Book findByTitle(String title);
+
+	public Optional<Book> findByTitleAndDescription(String title, String description);
 }
