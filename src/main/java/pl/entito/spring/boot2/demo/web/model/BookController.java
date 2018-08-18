2c8363bb50acc695fbe6abfcb7f658eb8ac390e2
Marek <killermm@gmail.com>
2018-08-18 22:46:29 +0200

    refactoring, rest request and properties demo moved to seperate classes

--- src/main/java/pl/entito/spring/boot2/demo/web/model/BookController.java
+++ src/main/java/pl/entito/spring/boot2/demo/web/model/BookController.java
@@ -2,9 +2,7 @@ package pl.entito.spring.boot2.demo.web.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,21 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BookController {
 
 	final private BookRepository repository;
 
-	@Value("${book.version}")
-	private String versionV1;
-
-	private String versionV2;
-
 	public BookController(BookRepository repository, BookProperties properties) {
 		this.repository = repository;
-		this.versionV2 = properties.getVersion();
 	}
 
 	@GetMapping("/")
@@ -59,32 +50,12 @@ public class BookController {
 		return "admin";
 	}
 
-	@GetMapping("/books")
-	@ResponseBody // to return books as JSON
 	private List<Book> fetchBooks() {
 		Iterable<Book> books = repository.findAll();
 		List<Book> bookWrapper = new ArrayList<>();
 		books.forEach(bookWrapper::add);
 
 		return bookWrapper;
-	}
-
-	@GetMapping("/version")
-	@ResponseBody
-	private Book fetchBooks2() {
-
-		String versionFromValue = "versionV1: " + versionV1;
-		String versionFromConstructor = "versionV2: " + versionV2;
-
-		return new Book(versionFromValue, versionFromConstructor);
-	}
-
-	@GetMapping("/book/{id}")
-	@ResponseBody // to return books as JSON
-	private Book fetchBook(@PathVariable long id) {
-		Optional<Book> book = repository.findById(id);
-
-		return book.orElse(new Book("Book", "Not found")); // TODO redirect to error page
 	}
 
 }
---
+++ src/main/java/pl/entito/spring/boot2/demo/web/model/BookRestController.java
@@ -0,0 +1,39 @@
+package pl.entito.spring.boot2.demo.web.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BookRestController {
+
+	final private BookRepository repository;
+
+	public BookRestController(BookRepository repository, BookProperties properties) {
+		this.repository = repository;
+	}
+
+	@GetMapping("/books")
+	@ResponseBody // to return books as JSON
+	private List<Book> fetchBooks() {
+		Iterable<Book> books = repository.findAll();
+		List<Book> bookWrapper = new ArrayList<>();
+		books.forEach(bookWrapper::add);
+
+		return bookWrapper;
+	}
+
+	@GetMapping("/book/{id}")
+	@ResponseBody // to return books as JSON
+	private Book fetchBook(@PathVariable long id) {
+		Optional<Book> book = repository.findById(id);
+
+		return book.orElse(new Book("Book", "Not found")); // TODO redirect to error page
+	}
+
+}
